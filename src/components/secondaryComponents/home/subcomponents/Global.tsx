01cb7c7a4d82d68ccd7569b41e154279dc665d8e
robertlyucra <rlujany@indracompany.com>
2024-08-23 18:26:43 -0500

    cambios en portada

--- src/components/secondaryComponents/home/subcomponents/Global.tsx
+++ src/components/secondaryComponents/home/subcomponents/Global.tsx
@@ -1,16 +1,44 @@
 import './styles/Global.scss'
 import Mapa from '../images/mapamundi.png'
+import { IonIcon } from '@ionic/react'
 
 const Global = () => {
   return (
     <div className="global">
       <div className="global_left">
-        <h1>Nuevas operaciones en el mundo</h1>
+        <div className='texts-descp' >
+          <h3>MAVRACON EN EL MUNDO</h3>
+          <h1>NUESTRAS OPERACIONES A NIVEL NMACIONAL</h1>
+          <p>En Mavracon somos mas que contructores, Nosotros somos diseñadores de tus sueños. Con un legado
+            de años de excelencia y experiencia, hemos sido pioneros de soluciones
+            innovadoras y pioneros en diversos megaproyectos.
+          </p>
+        </div>
+        <div className='status'>
+          <div className='status-projects'>
+            <IonIcon className='icon text-red-500' icon='ellipse' />
+            <p>PROYECTOS ACTUALES</p>
+          </div>
+          <div className='status-projects mid-status'>
+            <IonIcon className='icon text-blue-400' icon='ellipse' />
+            <p>PROYECTOS CULMINADOS</p>
+          </div>
+          <div className='status-projects'>
+            <IonIcon className='icon text-yellow-300' icon='ellipse' />
+            <p>SUBSIDIARIAS</p>
+          </div>
+        </div>
       </div>
 
       <div className="global_right">
-
-      <img src={Mapa}alt="" />
+        <div className='imagen'>
+          <img className='mapa' src={Mapa} alt="" />
+        </div>
+        <div className='bonotes'>
+          <div className='btn'>
+            <p>Perú</p>
+          </div>
+        </div>
       </div>
     </div>
   )
