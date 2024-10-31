3b716fca64f7be0e3d8e8c7f49d18930e82109d1
robertlyucra <rlujany@indracompany.com>
2024-10-31 18:08:48 -0500

    innovacion y sostenibilidad - logo-footer

--- src/components/maincomponents/footer/Footer.tsx
+++ src/components/maincomponents/footer/Footer.tsx
@@ -1,4 +1,6 @@
+import Logo from '../../../images/logo-oscuro.png';
 import "./Footer.scss"
+
 const Footer = () => {
   return (
     <div>
@@ -6,7 +8,7 @@ const Footer = () => {
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-              <img src="http://localhost:5173/src/images/logo-oscuro.png" className="h-16" alt="Flowbite Logo" />
+              <img src={Logo} className="h-16" alt="Flowbite Logo" />
             </a>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
               <li>
